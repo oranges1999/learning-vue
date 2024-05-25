025def2de3fc8bc849233590501475a30ef52a56
Huy <nhuhuy1999@gmail.com>
2024-05-25 16:18:22 +0700

    Same counter app, but more explicit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup>
 import { ref } from 'vue';
 
-  const count = ref(0);
+let count = ref(0);
+
+const addToCount=()=>{
+  count.value = count.value + 1;
+}
+
+const subtractToCount=()=>{
+  count.value = count.value - 1;
+}
 </script>
 
 <template>
@@ -11,8 +19,8 @@ import { ref } from 'vue';
         The current count is...
       </h4>
       <h1>{{ count }}</h1>
-      <button @click="count--">-</button>
-      <button @click="count++">+</button>
+      <button @click="subtractToCount">-</button>
+      <button @click="addToCount">+</button>
     </div>
   </main>
 </template>
