3f6a1ce343e63b9aecca65c034609f74696b687a
Huy <nhuhuy1999@gmail.com>
2024-05-25 16:28:59 +0700

    1h45m49s

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,16 @@
-<script setup>
-import { ref } from 'vue';
-
-let count = ref(0);
-
-const addToCount=()=>{
-  count.value = count.value + 1;
-}
-
-const subtractToCount=()=>{
-  count.value = count.value - 1;
-}
-</script>
-
 <template>
   <main>
-    <div>
-      <h4>
-        The current count is...
-      </h4>
-      <h1>{{ count }}</h1>
-      <button @click="subtractToCount">-</button>
-      <button @click="addToCount">+</button>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button>+</button>
+      </header>
+      <div class="card-container">
+        <div class="card">
+          <p class="main-text"></p>
+          <p class="date"></p>
+        </div>
+      </div>
     </div>
   </main>
 </template>
-
-<style scoped>
-  main{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-origin: aliceblue;
-    height: 100vh;
-    width: 100vw;
-  }
-  div{
-    text-align: center;
-  }
-</style>
-  
