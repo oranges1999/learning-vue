0ee0b09c65d54767e97723797a53dbe1f4793615
Huy <nhuhuy1999@gmail.com>
2024-05-29 09:27:47 +0700

    working on todo-app

---
+++ src/components/TodoCreator.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { reactive, ref, defineEmits } from 'vue';
+
+// for primitive datatype
+const todo = ref(""); 
+// for more complex datatype
+const todoState = reactive({ 
+    todo: ""
+}); 
+const msg = ref("")
+const emit = defineEmits(["create-todo"]);
+const createTodo = () => {
+    if (todo.value.length<3) {
+        return msg = "Too short, please check again"
+    }
+    emit("create-todo", todo.value)
+}
+</script>
+
+<template>
+    <div class="input-wrap">
+        <input v-model.trim="todo" type="text">
+        <p>{{ msg }}</p>
+        <button @click="createTodo()">Create</button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.input-wrap {
+  display: flex;
+  transition: 250ms ease;
+  border: 2px solid #41b080;
+
+  &:focus-within {
+    box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
+      0 -2px 4px -2px rgb(0 0 0 / 0.1);
+  }
+
+  input {
+    width: 100%;
+    padding: 8px 6px;
+    border: none;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  button {
+    padding: 8px 16px;
+    border: none;
+  }
+}
+</style>
---
+++ src/views/TodosView.vue
@@ -0,0 +1,38 @@
+<script setup>
+import TodoCreator from "@/components/TodoCreator.vue"
+import { ref } from "vue";
+import { uid } from "uid";
+
+const todoList = ref([]);
+const createTodo = (todo) => {
+  todoList.value.push({
+    id: uid(),
+    todo,
+    isCompleted:null,
+    isEditing:null
+  });
+}
+</script>
+
+<template>
+  <main>
+   <h1>Create Todo</h1>
+   <TodoCreator @create-todo="createTodo"/>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+}
+</style>
