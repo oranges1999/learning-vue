db0bf7836cd957d83e03db499f6aa0f9b9a738d3
Huy <nhuhuy1999@gmail.com>
2024-05-30 11:38:51 +0700

    Small update for easier to read

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import {createRouter, createWebHistory} from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import AboutView from "../views/AboutView.vue";
-import CarView from "../views/CarView.vue";
-import ContactView from "../views/ContactView.vue";
-import NotFoundView from "../views/404View.vue";
+import HomeView from "@/views/HomeView.vue";
+import AboutView from "@/views/AboutView.vue";
+import CarView from "@/views/CarView.vue";
+import ContactView from "@/views/ContactView.vue";
+import NotFoundView from "@/views/404View.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
